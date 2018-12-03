eeea3505ee92bf1ec846aff48422b706a038154d
Katrina Agustin <katrina.agustin@gmail.com>
2018-12-03 08:38:30 -0800

    removed dotenv on app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
-// if (process.env.NODE_ENV !== 'production') require('dotenv').load()
 
 app.use('/', require('./routes/auth'))
 app.use('/', require('./routes/users'))
